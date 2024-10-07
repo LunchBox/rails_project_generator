0194c4992d89e28a0ba080620a8a3cbaf8b08835
DanielCheang007 <93252061+DanielCheang007@users.noreply.github.com>
2024-10-07 22:21:34 +0800

    update the config but failed

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,15 @@ FROM ruby:3.2.5
 
 RUN apt update -qq && apt install -y build-essential
 
+# for postgres
+RUN apt install -y libpq-dev
+
+# for nokogiri
+RUN apt install -y libxml2-dev libxslt1-dev
+
+# for a JS runtime
+RUN apt install -y nodejs
+
 WORKDIR /app
 
 RUN gem install rails -v 7.2.1
--- create_project.sh
+++ create_project.sh
@@ -5,4 +5,7 @@ RAILS_PROJECT_NAME=rails_app
 
 docker build -t $DOCKER_IMAGE_NAME .
 
-docker run --rm -it -v ${PWD}/:/app $DOCKER_IMAGE_NAME rails new $RAILS_PROJECT_NAME --skip-bundle --minimal --api
+docker run --rm -it -v ${PWD}/:/app $DOCKER_IMAGE_NAME rails new $RAILS_PROJECT_NAME --skip-bundle --minimal --api --database=postgresql
+
+cp $RAILS_PROJECT_NAME/config/database.yml $RAILS_PROJECT_NAME/config/database.yml.sample
+cp ./database.yml $RAILS_PROJECT_NAME/config/database.yml
